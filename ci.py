985cf64c2cd84c89f7762cc003d1348b1c31f4c5
dpaddon <31183898+dpaddon@users.noreply.github.com>
2023-08-25 17:32:23 +0100

    update version number in test

--- ci.py
+++ ci.py
@@ -1,7 +1,7 @@
 import wandb
 
 wandb_version = wandb.__version__
-expected_version = "2.0"
+expected_version = "0.15.8"
 
 print(f"{wandb_version=}")
 assert wandb_version == expected_version, f"expected {expected_version}, got {wandb_version}"
